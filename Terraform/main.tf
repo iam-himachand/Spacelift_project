f2e77a8fa0255a760b84e47fe5e43905762ab5e9
iam-himachand <himachandmaditapu@gmail.com>
2026-07-08 21:19:56 +0530

    Initial commit for Terraform and Ansible

---
+++ Terraform/main.tf
@@ -0,0 +1,61 @@
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnet
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "terraform-ec2-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create 2 EC2 Instances
+resource "aws_instance" "web" {
+  count = 2
+
+  ami                    = "ami-091138d0f0d41ff90" # Amazon Linux 2023 (verify for your region)
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = "aws_keypair" # Replace with your actual key pair name
+
+  tags = {
+    Name = "Terraform-EC2-${count.index + 1}"
+  }
+}
+
+output "instance_public_ips" {
+  value = aws_instance.web[*].public_ip
+}
