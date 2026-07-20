b2ac92bd01dc0de99a9d2609ffd505fd001d63de
iam-himachand <himachandmaditapu@gmail.com>
2026-07-20 20:03:07 +0530

    Add generate_inventory.sh for Ansible inventory creation
    This script generates an Ansible inventory file for Ubuntu servers using provided AWS instances.

---
+++ Ansible/enterprise-server-build/playbooks/generate_inventory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod 600 /mnt/workspace/AWS_New_key.pem
+
+cat > /mnt/workspace/inventory.ini <<EOF
+[ubuntu_servers]
+EOF
+
+for instance in $(echo "$instances" | jq -r '.[]'); do
+  echo "$instance ansible_user=ubuntu ansible_ssh_private_key_file=/mnt/workspace/AWS_New_key.pem" >> /mnt/workspace/inventory.ini
+done
