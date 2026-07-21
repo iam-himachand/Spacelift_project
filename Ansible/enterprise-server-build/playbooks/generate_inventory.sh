031da7af4c12f6d194ff7127aef4283ea4be0d1c
iam-himachand <himachandmaditapu@gmail.com>
2026-07-21 19:33:09 +0530

    Enhance generate_inventory.sh to show variables
    Added environment variable display to inventory generation script.

--- Ansible/enterprise-server-build/playbooks/generate_inventory.sh
+++ Ansible/enterprise-server-build/playbooks/generate_inventory.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "===== VARIABLES ====="
+env | sort
+
+echo "===== instances ====="
+echo "$instances"
+
 chmod 600 /mnt/workspace/AWS_New_key.pem
 
 cat > /mnt/workspace/inventory.ini <<EOF
